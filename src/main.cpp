e59f25dda5917c710f79bae4401fbc7ef3a89f5b
Alexander Smirnov <alexsm.w@yandex.ru>
2024-11-24 16:36:18 +0300

    Add password entering function

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,16 @@
 #include <fstream>
 #include <sstream>
 
+#include <termios.h>
+#include <unistd.h>
+
 #include <nlohmann/json.hpp>
 #include <pqxx/pqxx>
 
 #define STATUS_SUCCESS 0
 
-void printConnString(std::ostream &out, const nlohmann::json &config);
+void printConnString(std::ostream &out, const nlohmann::json &config, const std::string &password);
+std::string readPassword(const std::string &prompt);
 
 int main(int argc, char **argv) {
     if (argc != 2) {
@@ -23,7 +27,7 @@ int main(int argc, char **argv) {
     auto sqlConfig = config.at("sql");
 
     std::stringstream connStr;
-    printConnString(connStr, sqlConfig);
+    printConnString(connStr, sqlConfig, readPassword("password: "));
      
     pqxx::connection conn(connStr.str());
     if (conn.is_open())
@@ -34,12 +38,30 @@ int main(int argc, char **argv) {
     return STATUS_SUCCESS;
 }
 
-void printConnString(std::ostream &out, const nlohmann::json &config) {
+std::string readPassword(const std::string &prompt) {
+    std::string password;
+    struct termios oldTermSettings, newTermSettings;
+
+    tcgetattr(STDIN_FILENO, &oldTermSettings);
+    newTermSettings = oldTermSettings; // get terminal settings
+    newTermSettings.c_lflag &= ~(ECHO); // disable echo
+    tcsetattr(STDIN_FILENO, TCSANOW, &newTermSettings); // apply settings
+
+    std::cout << prompt;
+    std::getline(std::cin, password);
+    std::cout << std::endl;
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldTermSettings); // restore old settings
+
+    return password;
+}
+
+void printConnString(std::ostream &out, const nlohmann::json &config, const std::string &password) {
     const std::string username = config.at("username");
     out
         << "postgresql://"
         << username << ':'
-        << "fdyadmn@"
+        << password << '@' 
         << config.value("host", "localhost") << ':'
         << config.value("port", 5432) << '/'
         << config.value("db-name", username)
